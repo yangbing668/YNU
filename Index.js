4efa7b1f0d5d521380c4e98b65defe89341d2cd8
Danielian <48437389+Danielian@users.noreply.github.com>
2019-11-23 15:35:50 +0800

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,35 @@
+function check() {
+    var Count = document.getElementById("Count");
+    Count.style.display="block";
+}
+function download() {
+	
+}
+function choose(){
+	
+}
+function authorLevel(){
+	
+}
+function StartYear(){
+	var startYear = document.getElementById("startYear");
+	var opt = null;
+	for(var i = 1970; i <=2019; i++)
+	{
+		opt = document.createElement("option");
+		opt.value = i;
+		opt.innerHTML = i;
+		startYear.appendChild(opt);
+	}
+}
+function EndYear(){
+	var endYear = document.getElementById("endYear");
+	var opt = null;
+	for(var i = 1970; i <=2019; i++)
+	{
+		opt = document.createElement("option");
+		opt.value = i;
+		opt.innerHTML = i;
+		endYear.appendChild(opt);
+	}
+}
